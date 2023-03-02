a689edc0ea0937e102697f29f010f563f33d7ed6
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-03-02 22:32:27 +0100

    user validator tested and implemented

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,22 +1,17 @@
 const { body, validationResult } = require("express-validator");
 
-const passwordMessage =
-  "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one number, and one symbol";
-
 const validateUser = [
   body("email").isEmail().withMessage("Please provide a vallid email adress"),
   body("password")
-    .notEmpty()
-    .withMessage("Password is required")
+    .isStrongPassword()
+    .withMessage(
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol"
+    )
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long")
-    .isUppercase({ min: 1 })
-    .withMessage(passwordMessage)
-    .isNumeric({ min: 1 })
-    .withMessage(passwordMessage)
-    .withMessage(passwordMessage),
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one symbol"),
   body("role_id").isLength(),
-  body("image").isString(),
   body("full_name").isLength(),
   body("username").isLength({ min: 4, max: 30 }),
   (req, res, next) => {
