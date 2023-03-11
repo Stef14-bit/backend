9b4f8856dc5c1874469b397ab19efab0bf10aba3
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-03-11 23:08:41 +0100

    fixed hash password bug and postuser validator

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -2,13 +2,15 @@ const { body, validationResult } = require("express-validator");
 
 const validateUser = [
   body("email").isEmail().withMessage("Please provide a valid email address"),
-
+  body("password")
+    .isStrongPassword({ minLength: 8 })
+    .withMessage(
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol"
+    ),
   body("role_id").isLength(),
   body("full_name").isLength(),
   body("username").isLength({ min: 4, max: 30 }),
   (req, res, next) => {
-    console.log(req.body._hashed_password);
-    console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(422).json({ validationErrors: errors.array() });
